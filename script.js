bc165f46e2d19691fd1305451e1ee74d2e0deba0
mrudang48-del <mrudang48@gmmail.com>
2026-01-18 21:06:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("hello world ");
+>> git init
+>> git add README.md
+>> git commit -m "first commit"
+>> git branch -M main
+>> git remote add origin https://github.com/mrudang48-del/code1.git
+>> git push -u origin main
